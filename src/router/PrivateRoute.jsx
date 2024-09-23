1a0c655e43985ea8d24e3c4e160674dba0283a2c
Hassan-Aletreby <hassaneletreby21@gmail.com>
2024-09-23 18:27:11 +0300

    some things

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
 const PrivateRoute = ({ children }) => {
   const authToken = localStorage.getItem("authToken");
 
-  return authToken ? children : <Navigate to="/login" />;
-};
+  if (!authToken) {
+    return <Navigate to="/login" />;
+  }
 
-export default PrivateRoute;
+  return children;
+};
